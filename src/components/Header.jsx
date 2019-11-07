01ebfe65ae2bee64451421e0c428696071b8f08e
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-11-07 08:56:54 +0000

    further styling added to improve mobile responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,9 @@ const Header = props => {
       <h1>
         NC <span className="text-primary">News</span>
       </h1>
-      <p>Logged in as: {props.username}</p>
+      <p>
+        Logged in as: <span className="text-primary">{props.username}</span>
+      </p>
     </div>
   );
 };
