82aa5ec245b0a6d1ccdf6ecf73a1fde5cce69eb8
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-11-07 12:45:02 +0000

    Added conditional loading message for comments list

--- src/components/ArticleComments.jsx
+++ src/components/ArticleComments.jsx
@@ -5,55 +5,60 @@ import "../css/ArticleComments.css";
 import Voter from "./Voter";
 
 class ArticleComments extends Component {
-  state = { comments: [], commentInput: "" };
+  state = { comments: [], commentInput: "", isLoading: true };
   render() {
-    const { comments, commentInput } = this.state;
+    const { comments, commentInput, isLoading } = this.state;
     return (
       <ul className="ArticleComments">
-        <h2>Post New Comment:</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            <textarea
-              type="text"
-              onChange={this.handleChange}
-              value={commentInput}
-              required
-            >
-              {" "}
-            </textarea>
-            <button>Submit</button>
-          </label>
-        </form>
-        <h2>Comments</h2>
-        {comments.map(comment => {
-          return (
-            <li key={comment.comment_id}>
-              <h2>Author: {comment.author}</h2>
-              <h4>Posted on: {helper.dateFormat(comment.created_at)}</h4>
-              <p>{comment.body}</p>
-              <Voter
-                type="comments"
-                id={comment.comment_id}
-                votes={comment.votes}
-              />
-              {this.props.username === comment.author && (
-                <button
-                  onClick={() => {
-                    this.removeComment(comment.comment_id);
-                  }}
+        {isLoading && <h2>Page Loading...</h2>}
+        {!isLoading && (
+          <div>
+            <h2>Post New Comment:</h2>
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                <textarea
+                  type="text"
+                  onChange={this.handleChange}
+                  value={commentInput}
+                  required
                 >
-                  Delete Comment
-                </button>
-              )}
-            </li>
-          );
-        })}
+                  {" "}
+                </textarea>
+                <button>Submit</button>
+              </label>
+            </form>
+            <h2>Comments</h2>
+            {comments.map(comment => {
+              return (
+                <li key={comment.comment_id}>
+                  <h2>Author: {comment.author}</h2>
+                  <h4>Posted on: {helper.dateFormat(comment.created_at)}</h4>
+                  <p>{comment.body}</p>
+                  <Voter
+                    type="comments"
+                    id={comment.comment_id}
+                    votes={comment.votes}
+                  />
+                  {this.props.username === comment.author && (
+                    <button
+                      onClick={() => {
+                        this.removeComment(comment.comment_id);
+                      }}
+                    >
+                      Delete Comment
+                    </button>
+                  )}
+                </li>
+              );
+            })}
+          </div>
+        )}
       </ul>
     );
   }
   componentDidMount() {
     api.getArticleComments(this.props.article_id).then(({ comments }) => {
-      this.setState({ comments });
+      this.setState({ comments, isLoading: false });
     });
   }
 
